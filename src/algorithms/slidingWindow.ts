99f675482334a58aa30a188eb1fd8977b7a2189c
akghosh111 <ghoshkiran2017@gmail.com>
2026-04-16 15:17:31 +0530

    add slidingwindow & tokenbucket boilerplate

---
+++ src/algorithms/slidingWindow.ts
@@ -0,0 +1,11 @@
+const store = new Map<string, { count: number; resetTime: number }>();
+
+export function slidingWindow(
+  key: string,
+  limit: number,
+  windowMs: number
+) {
+
+    
+
+}
---
+++ src/algorithms/tokenBucket.ts
@@ -0,0 +1,11 @@
+const store = new Map<string, { count: number; resetTime: number }>();
+
+export function tokenBucket(
+  key: string,
+  limit: number,
+  windowMs: number
+) {
+
+    
+
+}
